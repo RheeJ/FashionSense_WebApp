ddfe640f944df399c151f0fe436c2de551b63f72
sgp715 <7SebastianPerez@gmail.com>
2017-01-12 22:45:22 -0600

    cleaning up

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import url, include
 import views
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'classification', views.ClassificationView, base_name='classification')
+# router = routers.DefaultRouter()
 # router.register(r'test', TestViewSet)
 
 urlpatterns = [
     url(r'classification', views.ClassificationView.as_view()),
-	url(r'^', include(router.urls)),
-	# url(r'^view', test_view),
+    url(r'^', include(router.urls)),
+    # url(r'^view', test_view),
 ]
