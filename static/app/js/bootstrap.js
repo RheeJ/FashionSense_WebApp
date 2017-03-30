ead2bec1ebb4385feed6d1bc67d189e7960102ac
jsong1996 <jimmysong2018@u.northwestern.edu>
2017-03-30 16:41:59 -0500

    PARALLAX WORKING!

--- static/app/js/bootstrap.js
+++ static/app/js/bootstrap.js
@@ -8,6 +8,10 @@ if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
 +function ($) {
   'use strict';
   var version = $.fn.jquery.split(' ')[0].split('.')
