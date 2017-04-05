ce07b6372a57d3f0a738760e01c39ad9345a9e22
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-04-04 21:15:29 -0500

    pics and stuff

--- app/views.py
+++ app/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('app/index.html')
-    # host, port = request.get_host().split(':')
-    return HttpResponse(template.render())
+    template = None
+    if request.method == 'GET':
+        template = loader.get_template('app/index.html')
+        return HttpResponse(template.render())
+    elif request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        print form
+        if form.is_valid():
+            print form
+            return HttpResponseRedirect('/success/url/')
